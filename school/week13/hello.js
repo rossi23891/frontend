1ae01417416d6c787f8351649135f0362058eeac
rossi23891 <rossi23891@gmail.com>
2021-09-18 21:47:00 +0300

    add answers week14

--- school/week13/hello.js
+++ school/week13/hello.js
@@ -1,4 +1,6 @@
-function hello(){
+function hello() {
     let name = prompt("Как тебя зовут?", "world");
     alert(`Hello, ${name}!`);
 }
+
+let sayHi = () => alert(`Привет, ${prompt("Как тебя зовут?", "world")}`);
