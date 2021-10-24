3b6bcd7c47ffb7bd51cb233efa0da5769aa5d65c
rossi23891 <rossi23891@gmail.com>
2021-10-24 18:56:58 +0300

    cat 2

---
+++ school/week2/questionnaire/cat.js
@@ -0,0 +1,43 @@
+class Cat {
+    constructor(name, owner, ownerPhone, breed, feed, sex, comment, photo) {
+        this.name = name;
+        this.owner = owner;
+        this.ownerPhone = ownerPhone;
+        this.breed = breed;
+        this.feed = feed;
+        this.sex = sex;
+        this.comment = comment;
+        this.photo = photo;
+    }
+}
+
+function createCat() {
+    let name = document.getElementById('pet_name').value;
+    let owner = document.getElementById('owner').value;
+    let ownerPhone = document.getElementById('ownerphone').value;
+    let breed = getBreed();
+    let feed = getFeed();
+    let sex = document.querySelector('.sex:checked').value;
+    let comment = document.getElementById('comment').value;
+    let photo = document.querySelector('.photo').files[0].name;
+    let cat = new Cat(name, owner, ownerPhone, breed, feed, sex, comment, photo);
+    console.log(cat);
+    return cat;
+}
+
+function getBreed() {
+    let breed = document.querySelector('.breed1').value;
+    if (breed === 'Другая') {
+        return document.querySelector('.breed2').value;
+    }
+    return breed;
+}
+
+function getFeed() {
+    let feed = [];
+    let feedOptions = document.querySelectorAll('.feed:checked');
+    for (let feedOption of feedOptions) {
+        feed.push(feedOption.value);
+    }
+    return feed;
+}
